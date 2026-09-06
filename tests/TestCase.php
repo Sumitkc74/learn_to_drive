1b23455211bd06b7f555115e12d56f4a283a603f
Sumitkc74 <120349298+Sumitkc74@users.noreply.github.com>
2026-09-06 01:11:43 -0700

    Restore test seeding after database refresh

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,13 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Seed the database after RefreshDatabase runs its migrations.
+     */
+    protected $seed = true;
 }
